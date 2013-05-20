bda71bdcf6a997083c34eac62462f1e62706958f
yexi <yexijiang@gmail.com>
2013-05-20 18:22:59 -0400

    HAMA-681: Add test case TestSmallMultiLayerPerceptron

---
+++ ml/src/test/java/org/apache/hama/ml/perception/TestSmallMultiLayerPerceptron.java
@@ -0,0 +1,11 @@
+package org.apache.hama.ml.perception;
+
+public class TestSmallMultiLayerPerceptron {
+
+	/**
+	 * Write and read the parameters of MLP.
+	 */
+	public void testWriteReadMLP() {
+		
+	}
+}
